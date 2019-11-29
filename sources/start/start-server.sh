f760507ca3b7f06b7d462d2a0fb158acfee5af3a
zhenghao <dpzhenghao@dfl.com.cn>
2019-11-29 18:56:24 +0800

    update

--- sources/start/start-server.sh
+++ sources/start/start-server.sh
@@ -1,5 +1,8 @@
+# start exec
 cd /usr/local/azkaban3.80.1/azkaban-exec-server
 ./bin/start-exec.sh 
 curl -G "localhost:$(<./executor.port)/executor?action=activate" && echo
+
+# start web
 cd /usr/local/azkaban3.80.1/azkaban-web-server
 ./bin/start-web.sh 
